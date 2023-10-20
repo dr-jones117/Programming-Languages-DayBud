406bbba34d58146d8d0b0cc55cfa35694e089213
Jonah <jonah.morgan@mines.sdsmt.edu>
2023-10-19 21:28:15 -0600

    initialized 'DayBud' SwiftUI Project

---
+++ DayBud/DayBud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayBud
+//
+//  Created by Jonah Morgan on 10/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DayBud/DayBud/DayBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayBudApp.swift
+//  DayBud
+//
+//  Created by Jonah Morgan on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DayBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
