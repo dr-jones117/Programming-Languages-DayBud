d9d057e6f6edf2f43dbe2bffa0c3e2f30cfa2da2
Jonah <jonah.morgan@mines.sdsmt.edu>
2023-11-01 23:05:40 -0600

    finished starter to-do-list

---
+++ DayBud/DayBud/Models/TaskModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskModel.swift
+//  DayBud
+//
+//  Created by Jonah Morgan on 11/1/23.
+//
+
+import Foundation
---
+++ DayBud/DayBud/View Models/ToDoListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoListViewModel.swift
+//  DayBud
+//
+//  Created by Jonah Morgan on 11/1/23.
+//
+
+import Foundation
