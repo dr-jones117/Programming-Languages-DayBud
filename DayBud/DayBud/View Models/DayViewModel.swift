cb7441f1f95f42348631c4dd57ad541574ac1747
cehrisman <cehrisman@ymail.com>
2023-11-30 18:41:39 -0700

    Pushing changes

---
+++ DayBud/DayBud/View Models/DayViewModel.swift
@@ -0,0 +1,17 @@
+import Foundation
+import Combine
+
+class DayViewModel: ObservableObject {
+    @Published var date: Date
+    @Published var quoteText: String = "\"You are what you eat...\""
+    @Published var toDoList = ToDoListViewModel()
+    @Published var weatherInfo: String = ""
+    
+    
+    
+    init(date: Date) {
+        self.date = date
+        self.toDoList = ToDoListViewModel()
+    }
+    
+}
